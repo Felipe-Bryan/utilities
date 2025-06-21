85f55a52797aad9792887619d87042d98c3a7a1b
Felipe Bryan <felipe.bryan@live.com>
2025-06-21 16:26:10 -0300

    feat: apiAwaken

--- apiAwaken/src/api.service.ts
+++ apiAwaken/src/api.service.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://comanda-api-1.onrender.com',
-});
+const api = axios.create();
 
 export async function get() {
-  await api.get('vivoli/client');
+  await api.get('https://comanda-api-1.onrender.com/vivoli/client');
+
+  await api.get('https://api-9m8v.onrender.com/api/city');
 }
